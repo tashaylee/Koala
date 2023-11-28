148ba7e689399fb08f6f1840e32d528f869ecbcc
Tasha Lee <tashalee2k@gmail.com>
2023-11-28 09:44:07 -0500

    add comment to helpers

--- src/main/java/com/topbloc/codechallenge/Helpers.java
+++ src/main/java/com/topbloc/codechallenge/Helpers.java
@@ -3,7 +3,9 @@ package com.topbloc.codechallenge;
 import org.json.simple.JSONObject;
 
 public class Helpers {
-
+    /*
+     * Standardizes the structure of the response.
+     */
     public static String handleResponse(String message, Object data) {
         JSONObject apiResponse = new JSONObject();
 
